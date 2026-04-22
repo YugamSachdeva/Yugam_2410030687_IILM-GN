4d024908063a64391af0ab428e3b407b01fcb714
Yugam Sachdeva <sachdevayugam55@gmail.com>
2026-04-22 20:26:55 +0530

    Create  binary-search.c

---
+++ daa-lab-programs/binary-search.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] > target)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {10,20,30,40,50,60,70,80,90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 90;
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
